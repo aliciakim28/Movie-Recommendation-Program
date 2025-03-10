3c51beabb27142d67486ed9332d05e7dd1765121
yoojinleee <yoojin_lee1@brown.edu>
2025-03-10 16:58:28 -0400

    Debugging Welcome Page
    I had an error in line 16, and I realized that I did not define nameBox above. I added a uicontrol function for the nameBox and ran the program again. The welcome page appeared, and when I entered my name and DOB, the program proceeded to the next page. Now, we would have to add codes that load the database onto the program before we work on the search engine. We decided to work on separate scripts first and then combine the code to minimize confusion.

--- WelcomePage.m
+++ WelcomePage.m
@@ -7,6 +7,10 @@ fig = figure('Name', 'Movie Recommendation System', 'Position', [500 300 400 250
 
 uicontrol ('Style', 'text','String','Welcome to the Movie Recommendation System!', 'Position', [50 180 300 40], 'FontSize',12,'FontWeight','bold');
 
+uicontrol ('Style','text','String','Enter your name:', 'Position',[50 140 100 20], 'FontSize',10, 'HorizontalAlignment','left');
+
+nameBox = uicontrol ('Style','edit','Position',[160 140 180 25],'FontSize',10);
+
 uicontrol ('Style','text', 'String','Date of Birth (YYYY-MM-DD):', 'Position',[50 100 150 20], 'FontSize', 10,'HorizontalAlignment','left');
 
 dobBox = uicontrol('Style', 'edit', 'Position', [210 100 130 25], 'FontSize', 10);
