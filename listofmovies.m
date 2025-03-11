7ad9bde7c8f1dec3eb971c42805cbc73800203a4
aliciakim28 <alicia_kim@brown.edu>
2025-03-10 23:28:14 -0400

    Installing Kaggle
    I made some changes while trying to install the Kaggle dataset of the list of movies but am still having trouble figuring out how to get Matlab to recognize the file. I am currently trying to manually download the code from Kaggle but may have to go to office hours to fully debug my code.

--- listofmovies.m
+++ listofmovies.m
@@ -1,6 +1,17 @@
+import kagglehub
+
+pip install kaggle
+
+% Download latest version
+path = kagglehub.dataset_download("rajugc/imdb-movies-dataset-based-on-genre");
+
+print("Path to dataset files:", path)
+
 system("kaggle datasets download -d rajugc/imdb-movies-dataset-based-on-genre");
 
 unzip("imdb-movies-dataset-based-on-genre.zip");
 disp("Path to dataset files:");
 dataset = readtable("imdb-movies-dataset.csv");
-disp(dataset(1:5, :));
+disp(dataset(1:10, :));
+
+dir('*.zip')
