d130ed132d0a21b053b403379134fe76c9457eae
faithpark-git <faith_park@brown.edu>
2025-03-12 14:52:13 -0400

    new interactive search
    i tried to fix the old one (about an hour and a half)

--- newinteractiveMovieSearch.m
+++ newinteractiveMovieSearch.m
@@ -1,23 +1,4 @@
-function movies = listofmovies() %Defined the function, list of movies, with an output named movies
-folderPath = 'archive'; %Defined folderPath with 'archive', which is where all the CSV files are stored.
-csvFiles = dir(fullfile(folderPath, '*.csv')); %Created a path to store all the CSV files to the variable csvFiles.
 
-movies = {}; %Created an empty array movies to store the datafiles.
-
-for i=1:length(csvFiles) %Created a loop for all the .csv files in the variable csvFiles.
-    filename = fullfile(folderPath, csvFiles(i).name); %Stores the names of files in the variable filename.
-    opts = detectImportOptions(filename, 'TextType','string','VariableNamingRule','preserve'); %Ensure that texts are read as strings instead of char. Ensure that column names remain the same. Store the result in opts.
-    requiredColumns = {'movie_name', 'genre', 'director'}; %Extract the columns 'movie_name' 'genre' and 'director' from the dataset
-    existingColumns = ismember(requiredColumns,opts.VariableNames); %Check if the required columns are in the data files, and store the result in existingColumns as a logical array.
-
-    if all(existingColumns) %If all existingColumns are present in the CSV file
-        opts.SelectedVariableNames = requiredColumns; %Read the requiredColumns
-        dataTable = readtable(filename, opts); %Store the requiredColumns into a data table.
-
-        movies = [movies;table2cell(dataTable)]; %Change the table to a cell array and stacks it in the variable movies
-    end
-end
-end
 function interactiveMovieSearch()
 
 movies = listofmovies(); %load movie data 
@@ -80,4 +61,4 @@ if ~isempty(director)
 end 
 
 recommendations = unique([genreRecommendations; directorRecommendations], 'rows'); 
-end 
+end
