277e19801423066c5f0013a81c71733d1d5e75f9
aliciakim28 <alicia_kim@brown.edu>
2025-03-14 14:12:14 -0400

    Debugging Main Interactive Platform
    I worked with Yoojin for ~30 minutes to debug the main page code which allows the user to choose where they want to go.

--- InteractiveWithUser.m
+++ InteractiveWithUser.m
@@ -3,27 +3,21 @@
 while true
     disp('Movie Recommendation System');
     disp('1. Register');
-    disp('2. Login');
-    disp('3. Search by Genre');
-    disp('4. View Genre Ratings');
-    disp('5. Exit');
+    disp('2. Search by Genre');
+    disp('3. View Genre Ratings');
+    disp('4. Exit');
 
     choice = input('Enter your choice (1/2/3/4/5):');
 
     switch choice
         case 1
-            movie_recommendation_welcome();
+            WelcomePage;
         case 2
-            user = login_user();
-            if isempty(user)
-                continue;
-            end
-        case 3
             genre = input('Enter genre to search:', 's');
             search_by_genre(genre);
-        case 4
+        case 3
             visualize_genre_ratings();
-        case 5
+        case 4
             disp('Exiting program.');
             break;
         otherwise
