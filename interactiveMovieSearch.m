fb3978caf54db4536a78aaea4b99d96878119b04
yoojinleee <yoojin_lee1@brown.edu>
2025-03-17 17:39:28 -0400

    Updating the Search to Integrate Fuzzy Search
    I added codes so that even if the input for genre or the director is not correctly spaced, the approximate results will show using the formula for Levenshtein distance. This took me about 4 hours. I still need to edit the code so that even if the input is misspelled, the approximate outputs will show.

--- interactiveMovieSearch.m
+++ interactiveMovieSearch.m
@@ -17,7 +17,29 @@ userGenre = strtrim(string(input('', 's')));
 disp('Enter preferred director (or press Enter to skip):'); 
 userDirector = strtrim(string(input('', 's'))); 
 
-matches = findMatchingMovies(moviesTable, userGenre, userDirector); 
+fuzzyGenre = fuzzyMatch(userGenre, string(moviesTable.genre));
+fuzzyDirector = fuzzyMatch(userDirector, string(moviesTable.director));
+
+if userGenre ~= "" && fuzzyGenre ~= "" && userGenre ~= fuzzyGenre
+    disp(sprintf('Did you mean genre: %s? Showing results...', fuzzyGenre));
+end
+
+if userDirector ~= "" && fuzzyDirector ~= "" && userDirector ~= fuzzyDirector
+    disp(sprintf('Did you mean director: %s? Showing results...', fuzzyDirector));
+end
+
+matches = findMatchingMovies(moviesTable, fuzzyGenre, fuzzyDirector); 
+
+if isempty(matches)
+    disp('No exact matches found. Performing fuzzy search for similar director names...');
+    
+    fuzzyDirector = fuzzyMatch(userDirector, string(moviesTable.director));
+    
+    if fuzzyDirector ~= ""
+        disp(['Did you mean: ', fuzzyDirector, '? Showing results...']);
+        matches = findMatchingMovies(moviesTable, userGenre, fuzzyDirector);
+    end
+end
 
 if isempty(matches)
     disp('No exact matches found. Suggesting alternative recommendations...'); 
@@ -78,3 +100,59 @@ recommendations = [genreRecommendations; directorRecommendations];
 recommendations = recommendations(uniqueIdx,:);
 end 
 
+function cleanStr = normalizeInput(str)
+    str = lower(strtrim(str)); 
+    str = regexprep(str, '\s+', '');
+    cleanStr = string(str);
+end
+
+function bestMatch = fuzzyMatch(inputStr, itemList)
+    if inputStr == "" || isempty(itemList)
+        bestMatch = "";
+        return;
+    end
+
+    itemList = string(itemList);
+    itemList = itemList(~ismissing(itemList)); 
+
+    inputStr = normalizeInput(inputStr);
+    minDistance = inf;
+    bestMatch = "";
+
+    for i = 1:length(itemList)
+        dist = levenshteinDistance(char(inputStr), char(itemList(i)));
+        if dist < minDistance
+            minDistance = dist;
+            bestMatch = string(itemList(i));
+        end
+    end
+
+    if minDistance > 3  
+        bestMatch = "";
+    end
+end
+
+function d = levenshteinDistance(s1, s2)
+    s1 = char(s1);
+    s2 = char(s2);
+    
+    m = length(s1);
+    n = length(s2);
+    D = zeros(m+1, n+1);
+
+    for i = 1:m+1
+        D(i, 1) = i-1;
+    end
+    for j = 1:n+1
+        D(1, j) = j-1;
+    end
+
+    for i = 2:m+1
+        for j = 2:n+1
+            cost = ~(s1(i-1) == s2(j-1));
+            D(i, j) = min([D(i-1, j) + 1, D(i, j-1) + 1, D(i-1, j-1) + cost]);
+        end
+    end
+
+    d = D(m+1, n+1);
+end
